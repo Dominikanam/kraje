52ca7808cefa9c4d30cd9644fd22e4b3e2d4fb8a
Dominika Myszczynska <myszczynskad@gmail.com>
2019-06-06 12:53:58 +0100

    done

---
+++ react-router-sample/src/App.js
@@ -0,0 +1,35 @@
+// App.js:
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, Link, IndexLink, hashHistory } from 'react-router';
+
+const Navigation = props => (
+    <div>
+       <ul>
+	   		<Link to="/" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Home</Link>
+			<IndexLink to="/" activeStyle={{color: '#48abee'}} >Home</IndexLink>
+			<Link to="/contact" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Contact</Link>
+        </ul>
+        {props.children}
+    </div>
+);
+
+const PageNotFound = () => <h1>404 Not Found</h1>;
+const Hello = (props) => <h1>Witaj, {props.params.name}</h1>;
+
+class App extends Component {
+	render() {
+		return (
+			<Router history={hashHistory}>
+				<Route path='/' component={Navigation}>
+					<IndexRoute component={Home} />
+					<Route path='/contact' component={Contact} />
+					<Route path='/hello/:name' component={Hello} />
+					<Route path='*' component={PageNotFound} />
+				</Route>
+			</Router>
+		)
+	}
+}
+const Home = () => <h1>Hej, tu Home component</h1>
+const Contact = () => <h1>A tu Contact component</h1>
+export default App
